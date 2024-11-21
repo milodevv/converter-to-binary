f92eb43991bd2dc1f8b07ae73bd5dd5978295668
milodevv <milodev09@gmail.com>
2024-11-21 10:48:27 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+Covertidor a binarios
+
+Opciones
+    1. Convertir un numero
+    2. Convertir un caracter
+    3. Convertir una oracion
+
+Cumple con todas las validaciones para todos los casos u opciones presentadas
+
+Pasos para hacer las conversiones
+    Pasos para convertir de numero a binario
+        1. obtener el numero
+        2. realizar operacion de division para obtener sus mitades
+        3. reversar el resultado
+        4. hacer el calculo de los numeros del paso anterior para sacar los pares e impares
+        5. dar el resultado
+
+    Pasos para convertir de caracter a binario
+        1. obtener la letra
+        2. consultar su numeracion en decimal
+        3. convertir la numeracion a binario
+        4. dar el resultado
+        Ejemplo: para la letra (a) el resultado deberia ser: 1100001
+
+    Pasos para convertir de una oracion a binario
+        1. obtener la oracion
+        2. recorrer la oracion letra por letra
+        3. por cada letra recorrida consultar su numeracion en decimal
+        4. convertir a binario
+        5. hacer la concatenacion y dar el resultado
+
+        ejemplo de la oracion "miller" en binario
+        [1, 1, 0, 1, 1, 0, 1,
+         1, 1, 0, 1, 0, 0, 1,
+         1, 1, 0, 1, 1, 0, 0,
+         1, 1, 0, 1, 1, 0, 0,
+         1, 1, 0, 0, 1, 0, 1,
+         1, 1, 1, 0, 0, 1, 0]
---
+++ converter_to_binary.py
@@ -0,0 +1,110 @@
+import math
+from lists_of_values import ListsOfValues
+import time
+import inspect
+
+class ConvertToBinary():
+    count = 0
+    lists = ListsOfValues()
+
+    def __init__(self):
+        pass
+    
+    def values_to_convert(self, number: int):
+        self.lists.numbers.clear()
+        stack = inspect.stack()
+        caller = stack[1].function
+        while number >= 1:
+            self.count += 1
+            self.lists.numbers.append(number)
+            number /= 2
+            number = math.trunc(number)
+        new_list = self.lists.numbers.copy()
+        if caller == 'converter_of_sentence':
+            self.lists.numbers_copy.append(new_list)
+        new_list.reverse()
+        return new_list
+
+    def converter_of_number(self, number: int):
+        self.lists.numbers.clear()
+        if number <= 0:
+            return 0
+        elif number == 1:
+            return 1
+        else:
+            new_list = self.values_to_convert(number)
+            binary_numbers = []
+            for number in new_list:
+                binary_numbers.append(0) if number % 2 == 0 else binary_numbers.append(1)
+            return binary_numbers
+
+    def converter_of_character(self, char: str):
+        number_decimal = self.lists.characs_value[char]
+        new_list = self.values_to_convert(number_decimal)
+        binary_numbers = []
+        for number in new_list:
+            binary_numbers.append(0) if number % 2 == 0 else binary_numbers.append(1)
+        return binary_numbers
+
+    def converter_of_sentence(self, sentence: str):
+        self.lists.numbers_copy.clear()
+        new_list = []
+        binary_numbers = []
+        for word in sentence:
+            number_decimal = self.lists.characs_value[word]
+            new_list.append(self.values_to_convert(number_decimal))
+        for row in new_list:
+            for item in row:
+                binary_numbers.append(0) if item % 2 == 0 else binary_numbers.append(1)
+        return binary_numbers
+
+    def validate_response(self, response: str) -> bool:
+        return True if response in self.lists.correct_answers else False
+
+execution = True
+
+while execution:
+    try:
+        option = int(input("Opciones para ejecutar\n1. Convertir un numero\n2. Convertir un caracter\n3. Convertir una oracion\n4. Salir\nRespuesta: "))
+
+        converter = ConvertToBinary()
+
+        match option:
+            case 1:
+                number = int(input('Numero a convertir: '))
+                print(f'\nNumero ({number}) en binario: {converter.converter_of_number(number)}\nValores de la operacion {converter.lists.numbers}\nSe realizaron {converter.count} llamadas\n')
+                response = str(input('Desea continuar?: '))
+                if not converter.validate_response(response):
+                    break
+                else: continue
+
+            case 2:
+                charac = str(input('Caracter a convertir: '))
+                if len(charac) > 1:
+                    print('Debe digitar un solo caracter')
+                    time.sleep(2)
+                else:
+                    print(f'\nCaracter ({charac}) en bimario: {converter.converter_of_character(charac)}\nValores de la operacion {converter.lists.numbers}\nSe realizaron {converter.count} llamadas\n')
+                    response = str(input('Desea continuar?: '))
+                    if not converter.validate_response(response):
+                        break
+                    else: continue
+
+            case 3:
+                sentence = str(input('Oracion a convertir: '))
+                if 'ñ' in sentence:
+                    print('La letra "ñ" no cuenta con numeracion decimal')
+                    time.sleep(2)
+                else:
+                    print(f'\nOracion ({sentence}) en binario {converter.converter_of_sentence(sentence)}\nValores de la operacion {converter.lists.numbers_copy}\nSe realizaron {converter.count} llamadas\n')
+                    response = str(input('Desea continuar?: '))
+                    if not converter.validate_response(response):
+                        break
+                    else: continue
+
+            case 4:
+                break
+
+    except Exception as e:
+        print(f'Error de tipo: {e}')
+        time.sleep(2)
